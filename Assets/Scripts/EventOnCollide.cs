ffd8a10db3a1aaea698e06955f66c7847be06082
Maximilian Maroe <drakfyre@gmail.com>
2020-12-08 22:56:15 -0800

    Episode 1 complete!

---
+++ Assets/Scripts/EventOnCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnCollide : MonoBehaviour
+{
+    public UnityEvent buttonEvent;
+
+    void OnTriggerEnter()
+    {
+        buttonEvent.Invoke();
+    }
+}
