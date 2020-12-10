38e63976fd3cd5695fcc896582e67a9e78941b95
Maximilian Maroe <drakfyre@gmail.com>
2020-12-09 18:21:20 -0800

    End of Episode 1!  Button complete!

---
+++ Assets/Scripts/EventOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnTrigger : MonoBehaviour
+{
+    public UnityEvent buttonEvent;
+
+    void OnTriggerEnter()
+    {
+        buttonEvent.Invoke();
+    }
+}
